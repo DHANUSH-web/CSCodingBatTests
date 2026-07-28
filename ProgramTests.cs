6409d3e7925834578199a3b48d58d0eb9c6fe246
Dhanush H V <dhanushhv75@gmail.com>
2026-07-28 19:47:53 +0000

    CodingBat: Arrays::modThree

--- ProgramTests.cs
+++ ProgramTests.cs
@@ -473,4 +473,20 @@ public class ProgramTests
             Assert.False(Program.Has12(nums));
         }
     }
+
+    [Theory]
+    [InlineData(new int[] { 2, 1, 3, 5 }, true)]
+    [InlineData(new int[] { 2, 1, 2, 5 }, false)]
+    [InlineData(new int[] { 2, 4, 2, 5 }, true)]
+    public void ModThree_ReturnsExpected(int[] nums, bool expected)
+    {
+        if (expected)
+        {
+            Assert.True(Program.ModThree(nums));
+        }
+        else
+        {
+            Assert.False(Program.ModThree(nums));
+        }
+    }
 }
